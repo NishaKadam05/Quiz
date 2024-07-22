5d93fe0c81f14937a1b588d1751eb1bfcee79e14
Nisha Anandrao Kadam <142510987+NishaKadam05@users.noreply.github.com>
2024-07-22 17:25:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function correct(){
+    document.querySelector("#right").style.backgroundColor="Green";
+    document.querySelector("#right").style.border="1px solid Green";
+    document.querySelector("#crct").style.color="White";
+    document.querySelector("#crct").style.backgroundColor="Green";
+    document.querySelector("#crct").style.border="1px solid Green";
+}
+
+function wrongAns1(){
+    document.querySelector("#wrong1").style.backgroundColor="Red";
+    document.querySelector("#wrong1").style.border="1px solid Red";
+    document.querySelector("#incorrect1").style.color="White";
+    document.querySelector("#incorrect1").style.backgroundColor="Red";
+    document.querySelector("#incorrect1").style.border="1px solid Red";
+}
+
+function wrongAns2(){
+    document.querySelector("#wrong2").style.backgroundColor="Red";
+    document.querySelector("#wrong2").style.border="1px solid Red";
+    document.querySelector("#incorrect2").style.color="White";
+    document.querySelector("#incorrect2").style.backgroundColor="Red";
+    document.querySelector("#incorrect2").style.border="1px solid Red";
+
+
+}
+
+function wrongAns3(){
+    document.querySelector("#wrong3").style.backgroundColor="Red";
+    document.querySelector("#wrong3").style.border="1px solid Red";
+    document.querySelector("#incorrect3").style.color="White";
+    document.querySelector("#incorrect3").style.backgroundColor="Red";
+    document.querySelector("#incorrect3").style.border="1px solid Red";
+}
